27c388c416c7016f80019fe60f24e93afb129bbf
Derek Nguyen <ducanh.2206@gmail.com>
2021-09-18 20:27:08 +0700

    add run_miner.sh

---
+++ run_miner.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -a pool -b wallet -c worker"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "a:b:c:" opt
+do
+   case "$opt" in
+      a ) pool="$OPTARG" ;;
+      b ) wallet="$OPTARG" ;;
+      c ) worker="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$pool" ] || [ -z "$wallet" ] || [ -z "$worker" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+bash miner.sh -a $pool -b $wallet -c $worker & disown
